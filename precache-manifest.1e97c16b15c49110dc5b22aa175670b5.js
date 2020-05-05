4dfeca4f8e00e35048a63727afe491f4f34fa8ff
Jonathan Nakache <john.nakache@gmail.com>
2020-05-05 11:48:35 +0300

    Updates

---
+++ precache-manifest.1e97c16b15c49110dc5b22aa175670b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790828a903d45eb2294b46877c84b0a7",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d6aa250c2bfc22af2eca",
+    "url": "/pokedex/static/css/2.98e16827.chunk.css"
+  },
+  {
+    "revision": "a53be8b804f3ed942927",
+    "url": "/pokedex/static/css/main.221c3563.chunk.css"
+  },
+  {
+    "revision": "d6aa250c2bfc22af2eca",
+    "url": "/pokedex/static/js/2.1a557a37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.1a557a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53be8b804f3ed942927",
+    "url": "/pokedex/static/js/main.7a97816d.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  }
+]);
